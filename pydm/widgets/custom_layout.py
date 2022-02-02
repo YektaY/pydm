a8483858164a34bd1b021302f232f1920f66677b
YektaY <yektay1@gmail.com>
2022-02-02 04:55:39 -0800

    progress on custom layout for relative scaling, lots of bugs

--- pydm/widgets/custom_layout.py
+++ pydm/widgets/custom_layout.py
@@ -19,16 +19,27 @@ class PYDMLayout(QLayout):
         self.itemList = list()
         self.m_hSpace = horizontal_spacing
         self.m_vSpace = vertical_spacing
-
         self.setContentsMargins(margin, margin, margin, margin)
+        self.testRect = None
+        self.position_dict = {}
 
-    def addItem(self, item: QLayoutItem, position: tuple[int, int]):
+    '''
+    def addWidget(self, item: QWidget):
+        """
+        
+        Parameters
+        ----------
+        """
+        self.itemList.append(item)
+    '''
+
+    def addItem(self, item: QLayoutItem):
         """
 
         Parameters
         ----------
         """
-        self.itemList.append((item, position))  # you are re-writing this to take in the position of the object
+        self.itemList.append(item)
 
     def setGeometry(self, rect: QRect) -> None:
         """
@@ -38,7 +49,7 @@ class PYDMLayout(QLayout):
         """
 
         super().setGeometry(rect)
-        self.maintainLayout(rect, False)
+        self.maintainLayout(rect)
 
     def sizeHint(self) -> QSize:
         """
@@ -76,21 +87,13 @@ class PYDMLayout(QLayout):
         Parameters
         ----------
         """
+
         size = QSize()
         for item in self.itemList:
             size = size.expandedTo(item.minimumSize())
 
-        margins = self.contentsMargins()
-        size += QSize(margins.left() + margins.right(), margins.top() + margins.bottom())
+        size += QSize(self.spacing(), self.spacing())
         return size
-
-    '''
-    def __del__(self):
-        # copied for consistency, not sure this is needed or ever called
-        item = self.takeAt(0)
-        while item:
-            item = self.takeAt(0)
-    '''
 
     def horizontalSpacing(self) -> int:
         if self.m_hSpace >= 0:
@@ -108,7 +111,7 @@ class PYDMLayout(QLayout):
         return len(self.itemList)
 
     def heightForWidth(self, width: int) -> int:
-        height = self.maintainLayout(QRect(0, 0, width, 0), True)
+        height = self.maintainLayout(QRect(0, 0, width, 0))
         return height
 
     def smartSpacing(self, pm: QStyle.PixelMetric) -> int:
@@ -128,24 +131,158 @@ class PYDMLayout(QLayout):
     def hasHeightForWidth() -> bool:
         return True
 
-    def maintainLayout(self, rect: QRect, test_only: bool) -> int:
+    def maintainLayout(self, rect: QRect) -> int:
+        """
+        layouts the logic for scaling the window as the screen changes size.
+        This layout designed for relative scaling.
 
-        print("HCKAYAY!")
+        Parameters
+        ----------
+
+        """
+
+        print(rect.width(), rect.height())
+
+        effective_rect = self.contentsRect()
+        screen_ratio = effective_rect.width()/effective_rect.height()
+        height = 0
+
+        for item in self.itemList:
+            child_widget = item.widget()
+            #print(child_widget.width(), child_widget.height(), child_widget)
+            child_ratio = child_widget.width()/child_widget.height()
+            if screen_ratio > child_ratio:
+                width = child_widget.width()*effective_rect.height()/child_widget.height()
+                height = effective_rect.height()
+            else:
+                width = effective_rect.width()
+                height = child_widget.height()*effective_rect.width()/child_widget.width()
+
+            width = round(width)
+            height = round(height)
+            x = round(child_widget.x()*width/child_widget.width())
+            y = round(child_widget.y()*height/child_widget.height())
+
+            scaled_item = QRect(0, 0, width, height)
+            item.setGeometry(scaled_item)
+
+            if hasattr(child_widget, 'text'):
+                pass
+
+        return height
+
+
+
+    '''
+    def maintainLayout(self, rect: QRect) -> int:
+        """
+        layouts the logic for scaling the window as the screen changes size.
+
+
+        Parameters
+        ----------
+
+
+        """
+
+
+        if self.testRect is None:
+            self.testRect = rect
+
         left, top, right, bottom = self.getContentsMargins()
         effective_rect = rect.adjusted(+left, +top, -right, -bottom)
         x = effective_rect.x()
         y = effective_rect.y()
-        line_height = 0
 
-        for item in self.itemList:
-            # set position of object
-            wid = item.widget()
+        ratio_origin = self.testRect.width()/self.testRect.height()
+        ratio_changed = effective_rect.width()/effective_rect.height()
+
+
+        if ratio_origin > ratio_changed:
+            width_screen = effective_rect.width()
+            height_screen = width_screen/ratio_origin
+            x = 0
+            y = (effective_rect.height() - height_screen)/2
+        else:
+            height_screen = effective_rect.height()
+            width_screen = height_screen*ratio_origin
+            y = 0
+            x = (effective_rect.width() - width_screen)/2
+    
+        min = self.minimumSize()
+        h_ratio = effective_rect.height()/self.testRect.height()
+        w_ratio = effective_rect.width()/self.testRect.width()
+        print(rect.width(), self.testRect.width(), "ratio", w_ratio, h_ratio)
+
+        if h_ratio < 1:
+            h_ratio = 1
+
+        if w_ratio < 1:
+            w_ratio = 1
+
+        line_height = 0
+        
+        for index, item in enumerate(self.itemList):
+            child_widget = item.widget()
+
+            if index not in self.position_dict:
+                self.position_dict[index] = (child_widget.width(), child_widget.height())
+
+            space_x = self.horizontalSpacing()
+            space_y = self.verticalSpacing()
+
+            # determine new scale of main widget's child objects
+            width = child_widget.width() + effective_rect.width() - self.testRect.width()
+            height = child_widget.height() + effective_rect.height() - self.testRect.height()
+            #width = effective_rect.width()
+            #height = effective_rect.height()
+            print(width, child_widget.width(), effective_rect.width(), self.testRect.width())
+            position = QSize(width, height)
+
+            original_child_size = self.position_dict[index]
+            #print(original_child_size[0], original_child_size[1], child_widget)
+            x = child_widget.x()
+            y = y + height + space_y
+
+            # set new scale and position of child object
+            item.setGeometry(QRect(QPoint(int(x), int(y)), position))
+
+            height = item.sizeHint().height() * height_screen
+            width = item.sizeHint().width() * width_screen
+            height = int(height)
+            width = int(width)
+            pos = QSize(width, height)
+            
             space_x = self.horizontalSpacing()
             if space_x == -1:
-                space_x = wid.style().layoutSpacing(QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Horizontal)
+                space_x = child_widget.style().layoutSpacing(QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Horizontal)
+
             space_y = self.verticalSpacing()
             if space_y == -1:
-                space_y = wid.style().layoutSpacing(QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Vertical)
+                space_y = child_widget.style().layoutSpacing(QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Vertical)
+
+            next_x = x + pos.width() + space_x
+            
+            x = next_x
+            line_height = max(line_height, pos.height())
+
+            if next_x - space_x > effective_rect.right() and line_height > 0:
+                x = effective_rect.x()
+                y = y + pos.height() + space_y
+                next_x = x + pos.width() + space_x
+                line_height = 0
+
+
+            #item.widget().size().scale(width, height, Qt.KeepAspectRatio)
+
+
+            #child_widget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+            space_x = self.horizontalSpacing()
+            if space_x == -1:
+                space_x = child_widget.style().layoutSpacing(QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Horizontal)
+            space_y = self.verticalSpacing()
+            if space_y == -1:
+                space_y = child_widget.style().layoutSpacing(QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Vertical)
 
             next_x = x + item.sizeHint().width() + space_x
             if next_x - space_x > effective_rect.right() and line_height > 0:
@@ -159,5 +296,10 @@ class PYDMLayout(QLayout):
 
             x = next_x
             line_height = max(line_height, item.sizeHint().height())
+          
 
-        return y + line_height - rect.y() + bottom
+       #return y + line_height - rect.y() + bottom
+        return 0
+    
+    '''
+
