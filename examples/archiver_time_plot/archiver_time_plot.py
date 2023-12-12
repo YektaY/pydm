1014d45bcb0a4e37922de5a254ffd93932c00b65
YektaY <yektay1@gmail.com>
2023-12-11 17:00:38 -0800

    update test

--- examples/archiver_time_plot/archiver_time_plot.py
+++ examples/archiver_time_plot/archiver_time_plot.py
@@ -22,24 +22,48 @@ class archiver_time_plot_example(Display):
     def setup_ui(self):
         self.main_layout = QHBoxLayout()
         self.setLayout(self.main_layout)
-        self.plot = PyDMArchiverTimePlot(background=[255, 255, 255, 255])
-        self.main_layout.addWidget(self.plot)
-        self.plot.createCurveItem(
+        self.plot_live = PyDMArchiverTimePlot(background=[255, 255, 255, 255])
+        self.plot_archived = PyDMArchiverTimePlot(background=[255, 255, 255, 255])
+        self.main_layout.addWidget(self.plot_live)
+        self.main_layout.addWidget(self.plot_archived)
+
+        self.plot_live.createCurveItem(
             y_channel="ca://DEMO:ANGLE",
             name="name",
             color="red",
             plot_by_timestamps=True,
             yAxisName="Axis",
             useArchiveData=True,
-            noLiveData=True,
+            noLiveData=False,
         )
         # self.plot.addCurve(curve_item)
-        self.plot.addYChannel(
+        self.plot_live.addYChannel(
             y_channel="XCOR:LI29:302:IACT",
             name="name",
             color="red",
             yAxisName="Axis",
             useArchiveData=True,
+            noLiveData=False,
+        )
+
+        self.plot_archived.createCurveItem(
+            y_channel="ca://DEMO:ANGLE",
+            name="name",
+            color="blue",
+            plot_by_timestamps=True,
+            yAxisName="Axis",
+            useArchiveData=True,
             noLiveData=True,
         )
-        self.plot.updateXAxis(True)
+        # self.plot.addCurve(curve_item)
+        self.plot_archived.addYChannel(
+            y_channel="XCOR:LI29:302:IACT",
+            name="name",
+            color="blue",
+            yAxisName="Axis",
+            useArchiveData=True,
+            noLiveData=True,
+        )
+        self.plot_live.updateXAxis(True)
+
+
